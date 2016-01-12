0e6216c87276de31cc9ebdffb5dadc9f77f492f8
seb <seb@improuv.com>
2016-01-12 11:32:36 +0100

    added new Cucumber Steps file which contains missing glue code

---
+++ src/cucumbertest/java/com/improuv/xp/vendingmachine/Steps.java
@@ -0,0 +1,26 @@
+package com.improuv.xp.vendingmachine;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Steps {
+    @Given("^a filled vending machine$")
+    public void aFilledVendingMachine() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I press some button$")
+    public void iPressSomeButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^a can is thrown out$")
+    public void aCanIsThrownOut() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
