832fa3018b2407bcc85a3d33b0b809495de15fe2
seb <seb@improuv.com>
2016-01-12 11:45:33 +0100

    added minimal implementation to make acceptance test pass

--- src/cucumbertest/java/com/improuv/xp/vendingmachine/Steps.java
+++ src/cucumbertest/java/com/improuv/xp/vendingmachine/Steps.java
@@ -5,22 +5,24 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.assertTrue;
+
 public class Steps {
+
+    private VendingMachine vendingMachine;
+
     @Given("^a filled vending machine$")
     public void aFilledVendingMachine() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        vendingMachine = new VendingMachine();
     }
 
     @When("^I press some button$")
     public void iPressSomeButton() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        vendingMachine.pressButton();
     }
 
     @Then("^a can is thrown out$")
     public void aCanIsThrownOut() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        assertTrue(vendingMachine.threwOutCan());
     }
 }
---
+++ src/main/java/com/improuv/xp/vendingmachine/VendingMachine.java
@@ -0,0 +1,10 @@
+package com.improuv.xp.vendingmachine;
+
+public class VendingMachine {
+    public void pressButton() {
+    }
+
+    public boolean threwOutCan() {
+        return true;
+    }
+}
